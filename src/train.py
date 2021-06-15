f01e4d30d65a4d360db1cc6bf260af69570ef509
Lars Voegtlin <lars.voegtlin@unifr.ch>
2021-06-15 15:52:59 +0200

    :construction: start idea for the train/test routines

--- src/train.py
+++ src/train.py
@@ -73,14 +73,15 @@ def train(config: DictConfig) -> Optional[float]:
         logger=logger,
     )
 
-    # Train the model
-    log.info("Starting training!")
-    trainer.fit(model=task, datamodule=datamodule)
+    if config.train:
+        # Train the model
+        log.info("Starting training!")
+        trainer.fit(model=task, datamodule=datamodule)
 
     # Evaluate model on test set after training
-    if not config.trainer.get("fast_dev_run"):
+    if config.test:
         log.info("Starting testing!")
-        trainer.test()
+        trainer.test(model=task, datamodule=datamodule)
 
     # Make sure everything closed properly
     log.info("Finalizing!")
