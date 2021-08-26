427ecad3bca71fced86e2fa3244303e81fa107f2
Lars Voegtlin <lars.voegtlin@unifr.ch>
2021-08-26 10:39:36 +0200

    :memo: some documentation for step

--- src/tasks/base_task.py
+++ src/tasks/base_task.py
@@ -57,9 +57,16 @@ class AbstractTask(LightningModule, metaclass=ABCMeta):
         self.test_output_path = test_output_path
         self.save_hyperparameters()
 
-    def step(self, batch: Any, batch_idx: int) -> Any:
+    def step(self, batch: Any, batch_idx: int, metric_kwargs: Optional[Dict[str, Dict[str, Any]]] = None) -> Any:
         """
         The training/validation/test step. Override for custom behavior.
+        Args:
+            batch: the batch with the images (x) and the gt (y) in the order (x, y)
+            batch_idx: the index of the given batch
+            metric_kwargs: a dictionary with a entry with the additional arguments (pred and y always provided).
+                e.g. you have two metrics (A, B) and B takes an additional arguments x and y so the dictionary would
+                look like this: {'B': {'x': 'value', 'y': 'value'}}
+
         """
         x, y = batch
         y_hat = self(x)
@@ -70,10 +77,10 @@ class AbstractTask(LightningModule, metaclass=ABCMeta):
         y_hat = self.to_metrics_format(output["y_hat"])
         for name, metric in self.metrics.items():
             if isinstance(metric, torchmetrics.metric.Metric):
-                metric(y_hat, y)
+                metric(y_hat, y, **metric_kwargs[name])
                 logs[name] = metric  # log the metric itself if it is of type Metric
             else:
-                logs[name] = metric(y_hat, y)
+                logs[name] = metric(y_hat, y, **metric_kwargs[name])
         logs.update(losses)
         if len(losses.values()) > 1:
             logs["total_loss"] = sum(losses.values())
@@ -92,18 +99,18 @@ class AbstractTask(LightningModule, metaclass=ABCMeta):
     def forward(self, x: Any) -> Any:
         return self.model(x)
 
-    def training_step(self, batch: Any, batch_idx: int) -> Any:
+    def training_step(self, batch: Any, batch_idx: int, **kwargs) -> Any:
         output = self.step(batch, batch_idx)
         self.log_dict({f"train_{k}": v for k, v in output["logs"].items()}, on_step=True, on_epoch=True, prog_bar=True,
                       sync_dist=True)
         return output["loss"]
 
-    def validation_step(self, batch: Any, batch_idx: int) -> None:
+    def validation_step(self, batch: Any, batch_idx: int, **kwargs) -> None:
         output = self.step(batch, batch_idx)
         self.log_dict({f"val_{k}": v for k, v in output["logs"].items()}, on_step=False, on_epoch=True, prog_bar=True,
                       sync_dist=True)
 
-    def test_step(self, batch: Any, batch_idx: int) -> None:
+    def test_step(self, batch: Any, batch_idx: int, **kwargs) -> None:
         output = self.step(batch, batch_idx)
         self.log_dict({f"test_{k}": v for k, v in output["logs"].items()}, on_step=False, on_epoch=True, prog_bar=True,
                       sync_dist=True)
