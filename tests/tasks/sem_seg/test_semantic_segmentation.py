adcd391750a4f1492fa11a9c096ee43d43b6dc57
Lars Voegtlin <lars.voegtlin@unifr.ch>
2021-10-05 10:07:30 +0200

    :white_check_mark: global test now also running

--- tests/tasks/sem_seg/test_semantic_segmentation.py
+++ tests/tasks/sem_seg/test_semantic_segmentation.py
@@ -9,7 +9,7 @@ from src.tasks.semantic_segmentation.semantic_segmentation import SemanticSegmen
 from tests.datamodules.hisDBDataModule.dummy_data.dummy_data import data_dir_cropped
 
 
-def test_semantic_segmentation(data_dir_cropped):
+def test_semantic_segmentation(data_dir_cropped, tmp_path):
     seed_everything(42)
 
     # datamodule
@@ -22,29 +22,22 @@ def test_semantic_segmentation(data_dir_cropped):
 
     model = baby_unet()
     segmentation = SemanticSegmentation(model=model,
-                                        optimizer=torch.optim.Adam(params=model.parameters(),
-                                                                   lr=1e-3,
-                                                                   betas=[0.9, 0.999],
-                                                                   eps=1e-8,
-                                                                   weight_decay=0,
-                                                                   amsgrad=False),
-                                        loss_fn=torch.nn.CrossEntropyLoss()
+                                        optimizer=torch.optim.Adam(params=model.parameters()),
+                                        loss_fn=torch.nn.CrossEntropyLoss(),
+                                        test_output_path=tmp_path
                                         )
 
     # different paths needed later
-    analysis_path = segmentation.test_output_path / 'analysis'
     patches_path = segmentation.test_output_path / 'patches'
     test_data_patch = data_dir_cropped / 'test' / 'data'
 
     trainer = pl.Trainer(max_epochs=2, log_every_n_steps=10,
                          default_root_dir=segmentation.test_output_path, accelerator='ddp_cpu')
 
-    # TODO fix numbers again
-    assert 1 == trainer.fit(segmentation, datamodule=data_module)
+    trainer.fit(segmentation, datamodule=data_module)
 
     results = trainer.test()
-    assert results[0]['test-hisdb-iou'] > 38
-    assert results[0]['test-hisdb-iou'] < 41
-    assert analysis_path.exists()
-    assert len(list(analysis_path.glob('*.png'))) == 5
+    print(results)
+    assert results[0]['test/crossentropyloss'] == 1.0625288486480713
+    assert results[0]['test/crossentropyloss_epoch'] == 1.0625288486480713
     assert len(list(patches_path.glob('*/*.npy'))) == len(list(test_data_patch.glob('*/*.png')))
