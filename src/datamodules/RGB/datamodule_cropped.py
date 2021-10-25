530270222565ce21ac59d53f1a414a402410c3e1
Paul M <paul.maergner@gmail.com>
2021-10-25 17:36:16 +0200

    :construction: initial datamodule files for RGB gt

---
+++ src/datamodules/RGB/datamodule_cropped.py
@@ -0,0 +1,141 @@
+from pathlib import Path
+from typing import Union, List, Optional
+
+from torch.utils.data import DataLoader
+from torchvision import transforms
+
+from src.datamodules.base_datamodule import AbstractDatamodule
+from src.datamodules.RGB.datasets.cropped_dataset import CroppedHisDBDataset
+from src.datamodules.RGB.utils.image_analytics import get_analytics
+from src.datamodules.RGB.utils.misc import validate_path_for_segmentation
+from src.datamodules.RGB.utils.twin_transforms import TwinRandomCrop, OneHotEncoding, OneHotToPixelLabelling
+from src.datamodules.RGB.utils.wrapper_transforms import OnlyImage, OnlyTarget
+from src.utils import utils
+
+log = utils.get_logger(__name__)
+
+
+class DivaHisDBDataModuleCropped(AbstractDatamodule):
+    def __init__(self, data_dir: str = None, data_folder_name: str = 'data', gt_folder_name: str = 'gt',
+                 selection_train: Optional[Union[int, List[str]]] = None,
+                 selection_val: Optional[Union[int, List[str]]] = None,
+                 selection_test: Optional[Union[int, List[str]]] = None,
+                 crop_size: int = 256, num_workers: int = 4, batch_size: int = 8,
+                 shuffle: bool = True, drop_last: bool = True):
+        super().__init__()
+
+        analytics = get_analytics(input_path=Path(data_dir),
+                                  get_gt_data_paths_func=CroppedHisDBDataset.get_gt_data_paths)
+
+        self.mean = analytics['mean']
+        self.std = analytics['std']
+        self.class_encodings = analytics['class_encodings']
+        self.num_classes = len(self.class_encodings)
+        self.class_weights = analytics['class_weights']
+
+        self.image_transform = OnlyImage(transforms.Compose([transforms.ToTensor(),
+                                                             transforms.Normalize(mean=self.mean, std=self.std)]))
+        self.target_transform = OnlyTarget(transforms.Compose([
+            # transforms the gt image into a one-hot encoded matrix
+            OneHotEncoding(class_encodings=self.class_encodings),
+            # transforms the one hot encoding to argmax labels -> for the cross-entropy criterion
+            OneHotToPixelLabelling()]))
+        self.twin_transform = TwinRandomCrop(crop_size=crop_size)
+
+        self.num_workers = num_workers
+        self.batch_size = batch_size
+
+        self.shuffle = shuffle
+        self.drop_last = drop_last
+
+        self.data_folder_name = data_folder_name
+        self.gt_folder_name = gt_folder_name
+        self.data_dir = validate_path_for_segmentation(data_dir=data_dir, data_folder_name=self.data_folder_name,
+                                                       gt_folder_name=self.gt_folder_name)
+
+        self.selection_train = selection_train
+        self.selection_val = selection_val
+        self.selection_test = selection_test
+
+        self.dims = (3, crop_size, crop_size)
+
+    def setup(self, stage: Optional[str] = None):
+        super().setup()
+        if stage == 'fit' or stage is None:
+            self.train = CroppedHisDBDataset(**self._create_dataset_parameters('train'), selection=self.selection_train)
+            self.val = CroppedHisDBDataset(**self._create_dataset_parameters('val'), selection=self.selection_val)
+
+            self._check_min_num_samples(num_samples=len(self.train), data_split='train',
+                                        drop_last=self.drop_last)
+            self._check_min_num_samples(num_samples=len(self.val), data_split='val',
+                                        drop_last=self.drop_last)
+
+        if stage == 'test' or stage is not None:
+            self.test = CroppedHisDBDataset(**self._create_dataset_parameters('test'), selection=self.selection_test)
+            # self._check_min_num_samples(num_samples=len(self.test), data_split='test',
+            #                             drop_last=False)
+
+    def _check_min_num_samples(self, num_samples: int, data_split: str, drop_last: bool):
+        num_processes = self.trainer.num_processes
+        batch_size = self.batch_size
+        if drop_last:
+            if num_samples < (self.trainer.num_processes * self.batch_size):
+                log.error(
+                    f'#samples ({num_samples}) in "{data_split}" smaller than '
+                    f'#processes({num_processes}) times batch size ({batch_size}). '
+                    f'This only works if drop_last is false!')
+                raise ValueError()
+        else:
+            if num_samples < (self.trainer.num_processes * self.batch_size):
+                log.warning(
+                    f'#samples ({num_samples}) in "{data_split}" smaller than '
+                    f'#processes ({num_processes}) times batch size ({batch_size}). '
+                    f'This works due to drop_last=False, however samples will occur multiple times. '
+                    f'Check if this behavior is intended!')
+
+    def train_dataloader(self, *args, **kwargs) -> DataLoader:
+        return DataLoader(self.train,
+                          batch_size=self.batch_size,
+                          num_workers=self.num_workers,
+                          shuffle=self.shuffle,
+                          drop_last=self.drop_last,
+                          pin_memory=True)
+
+    def val_dataloader(self, *args, **kwargs) -> Union[DataLoader, List[DataLoader]]:
+        return DataLoader(self.val,
+                          batch_size=self.batch_size,
+                          num_workers=self.num_workers,
+                          shuffle=self.shuffle,
+                          drop_last=self.drop_last,
+                          pin_memory=True)
+
+    def test_dataloader(self, *args, **kwargs) -> Union[DataLoader, List[DataLoader]]:
+        return DataLoader(self.test,
+                          batch_size=self.batch_size,
+                          num_workers=self.num_workers,
+                          shuffle=False,
+                          drop_last=False,
+                          pin_memory=True)
+
+    def _create_dataset_parameters(self, dataset_type: str = 'train'):
+        is_test = dataset_type == 'test'
+        return {'path': self.data_dir / dataset_type,
+                'image_transform': self.image_transform,
+                'target_transform': self.target_transform,
+                'twin_transform': self.twin_transform,
+                'classes': self.class_encodings,
+                'is_test': is_test}
+
+    def get_img_name_coordinates(self, index):
+        """
+        Returns the original filename of the crop and its coordinate based on the index.
+        You can just use this during testing!
+        :param index:
+        :return:
+        """
+        if not hasattr(self, 'test'):
+            raise Exception('This method can just be called during testing')
+
+        return self.test.img_paths_per_page[index][2:]
+
+
---
+++ src/datamodules/RGB/utils/output_tools.py
@@ -0,0 +1,118 @@
+from pathlib import Path
+from typing import Union
+
+import numpy as np
+import torch
+from PIL import Image
+
+
+def _get_argmax(output: Union[torch.Tensor, np.ndarray], dim=1):
+    """
+    takes the biggest value from a pixel across all classes
+    :param output: (Batch_size x num_classes x W x H)
+        matrix with the given attributes
+    :return: (Batch_size x W x H)
+        matrix with the hisdb class number for each pixel
+    """
+    if isinstance(output, torch.Tensor):
+        return torch.argmax(output, dim=dim)
+    if isinstance(output, np.ndarray):
+        return np.argmax(output, axis=dim)
+    return output
+
+
+def merge_patches(patch, coordinates, full_output):
+    """
+    This function merges the patch into the full output image
+    Overlapping values are resolved by taking the max.
+
+    Parameters
+    ----------
+    patch: numpy matrix of size [#classes x crop_size x crop_size]
+        a patch from the larger image
+    coordinates: tuple of ints
+        top left coordinates of the patch within the larger image for all patches in a batch
+    full_output: numpy matrix of size [#C x H x W]
+        output image at full size
+    Returns
+    -------
+    full_output: numpy matrix [#C x Htot x Wtot]
+    """
+    assert len(full_output.shape) == 3
+    assert full_output.size != 0
+
+    # Resolve patch coordinates
+    x1, y1 = coordinates
+    x2, y2 = x1 + patch.shape[2], y1 + patch.shape[1]
+
+    # If this triggers it means that a patch is 'out-of-bounds' of the image and that should never happen!
+    assert x2 <= full_output.shape[2]
+    assert y2 <= full_output.shape[1]
+
+    mask = np.isnan(full_output[:, y1:y2, x1:x2])
+    # if still NaN in full_output just insert value from crop, if there is a value then take max
+    full_output[:, y1:y2, x1:x2] = np.where(mask, patch, np.maximum(patch, full_output[:, y1:y2, x1:x2]))
+
+    return full_output
+
+
+def save_output_page_image(image_name, output_image, output_folder: Path, class_encoding):
+    """
+    Helper function to save the output during testing in the DIVAHisDB format
+
+    Parameters
+    ----------
+    image_name: str
+        name of the image that is saved
+    output_image: numpy matrix of size [#C x H x W]
+        output image at full size
+    output_folder: Path
+        path to the output folder for the test data
+    class_encoding: list(int)
+        list with the class encodings
+
+    Returns
+    -------
+    mean_iu : float
+        mean iu of this image
+    """
+
+    output_encoded = output_to_class_encodings(output_image, class_encoding)
+
+    dest_folder = output_folder
+    dest_folder.mkdir(parents=True, exist_ok=True)
+    dest_filename = dest_folder / image_name
+
+    # Save the output
+    Image.fromarray(output_encoded.astype(np.uint8)).save(str(dest_filename))
+
+
+def output_to_class_encodings(output, class_encodings, perform_argmax=True):
+    """
+    This function converts the output prediction matrix to an image like it was provided in the ground truth
+
+    Parameters
+    -------
+    output : np.array of size [#C x H x W]
+        output prediction of the network for a full-size image, where #C is the number of classes
+    class_encodings : List
+        Contains the range of encoded classes
+    perform_argmax : bool
+        perform argmax on input data
+    Returns
+    -------
+    numpy array of size [C x H x W] (BGR)
+    """
+
+    B = np.argmax(output, axis=0) if perform_argmax else output
+
+    class_to_B = {i: j for i, j in enumerate(class_encodings)}
+
+    masks = [B == old for old in class_to_B.keys()]
+
+    for mask, (old, new) in zip(masks, class_to_B.items()):
+        B = np.where(mask, new, B)
+
+    rgb = np.dstack((np.zeros(shape=(B.shape[0], B.shape[1], 2), dtype=np.int8), B))
+
+    return rgb
