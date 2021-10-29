:white_check_mark: fixed rotation test

src/datamodules/DivaHisDB/utils/image_analytics.py
@@ -24,37 +24,72 @@ def get_analytics(input_path: Path, data_folder_name: str, gt_folder_name: str, 
     Returns
     -------
     """
-    analytics_file_path = input_path / 'analytics.json'
-    if analytics_file_path.exists():
-        with analytics_file_path.open(mode='r') as f:
-            analytics_dict = json.load(fp=f)
-    else:
+    expected_keys_data = ['mean', 'std']
+    expected_keys_gt = ['class_weights', 'class_encodings']
+
+    analytics_path_data = input_path / f'analytics.data.{data_folder_name}.json'
+    analytics_path_gt = input_path / f'analytics.gt.hisDB.{gt_folder_name}.json'
+
+    analytics_data = None
+    analytics_gt = None
+
+    missing_analytics_data = True
+    missing_analytics_gt = True
+
+    if analytics_path_data.exists():
+        with analytics_path_data.open(mode='r') as f:
+            analytics_data = json.load(fp=f)
+        # check if analytics file is complete
+        if all(k in analytics_data for k in expected_keys_data):
+            missing_analytics_data = False
+
+    if analytics_path_gt.exists():
+        with analytics_path_gt.open(mode='r') as f:
+            analytics_gt = json.load(fp=f)
+        # check if analytics file is complete
+        if all(k in analytics_gt for k in expected_keys_gt):
+            missing_analytics_gt = False
+
+    if missing_analytics_data or missing_analytics_gt:
         train_path = input_path / 'train'
         gt_data_path_list = get_gt_data_paths_func(train_path, data_folder_name=data_folder_name,
                                                    gt_folder_name=gt_folder_name)
         file_names_data = np.asarray([str(item[0]) for item in gt_data_path_list])
         file_names_gt = np.asarray([str(item[1]) for item in gt_data_path_list])
-        mean, std = compute_mean_std(file_names=file_names_data, **kwargs)
 
-        # Measure weights for class balancing
-        logging.info(f'Measuring class weights')
-        # create a list with all gt file paths
-        class_weights, class_encodings = _get_class_frequencies_weights_segmentation(gt_images=file_names_gt, **kwargs)
-        analytics_dict = {'mean': mean.tolist(),
-                          'std': std.tolist(),
-                          'class_weights': class_weights.tolist(),
-                          'class_encodings': class_encodings.tolist()}
-        # save json
-        try:
-            with analytics_file_path.open(mode='w') as f:
-                json.dump(obj=analytics_dict, fp=f)
-        except IOError as e:
-            if e.errno == errno.EACCES:
-                print(f'WARNING: No permissions to write analytics file ({analytics_file_path})')
-            else:
-                raise
-    # returns the 'mean[RGB]', 'std[RGB]', 'class_frequencies_weights[num_classes]', 'class_encodings'
-    return analytics_dict
+        if missing_analytics_data:
+            mean, std = compute_mean_std(file_names=file_names_data, **kwargs)
+            analytics_data = {'mean': mean.tolist(),
+                              'std': std.tolist()}
+            # save json
+            try:
+                with analytics_path_data.open(mode='w') as f:
+                    json.dump(obj=analytics_data, fp=f)
+            except IOError as e:
+                if e.errno == errno.EACCES:
+                    print(f'WARNING: No permissions to write analytics file ({analytics_path_data})')
+                else:
+                    raise
+
+        if missing_analytics_gt:
+            # Measure weights for class balancing
+            logging.info(f'Measuring class weights')
+            # create a list with all gt file paths
+            class_weights, class_encodings = _get_class_frequencies_weights_segmentation_hisdb(gt_images=file_names_gt,
+                                                                                               **kwargs)
+            analytics_gt = {'class_weights': class_weights.tolist(),
+                            'class_encodings': class_encodings.tolist()}
+            # save json
+            try:
+                with analytics_path_gt.open(mode='w') as f:
+                    json.dump(obj=analytics_gt, fp=f)
+            except IOError as e:
+                if e.errno == errno.EACCES:
+                    print(f'WARNING: No permissions to write analytics file ({analytics_path_gt})')
+                else:
+                    raise
+
+    return analytics_data, analytics_gt
 
 
 def compute_mean_std(file_names: List[Path], inmem=False, workers=4, **kwargs):
@@ -295,7 +330,7 @@ def get_class_weights_graphs(dataset, **kwargs):
     return class_weights
 
 
-def _get_class_frequencies_weights_segmentation(gt_images, **kwargs):
+def _get_class_frequencies_weights_segmentation_hisdb(gt_images, **kwargs):
     """
     Get the weights proportional to the inverse of their class frequencies.
     The vector sums up to 1
@@ -333,5 +368,4 @@ def _get_class_frequencies_weights_segmentation(gt_images, **kwargs):
 
 
 if __name__ == '__main__':
-    # print(get_analytics(input_path=Path('/netscratch/datasets/semantic_segmentation/datasets/CB55/'), inmem=True, workers=16))
     print(get_analytics(input_path=Path('tests/dummy_data/dummy_dataset')))

src/datamodules/RotNet/datamodule_cropped.py
@@ -5,7 +5,7 @@ import numpy as np
 from torch.utils.data import DataLoader
 from torchvision import transforms
 
-from src.datamodules.RotNet.utils.image_analytics import get_analytics
+from src.datamodules.RotNet.utils.image_analytics import get_analytics_data
 from src.datamodules.RotNet.datasets.cropped_dataset import CroppedRotNet, ROTATION_ANGLES
 from src.datamodules.RotNet.utils.misc import validate_path_for_self_supervised
 from src.datamodules.RotNet.utils.wrapper_transforms import OnlyImage
@@ -16,7 +16,7 @@ log = utils.get_logger(__name__)
 
 
 class RotNetDivaHisDBDataModuleCropped(AbstractDatamodule):
-    def __init__(self, data_dir: str = None, data_folder_name: str = 'data',
+    def __init__(self, data_dir: str, data_folder_name: str,
                  selection_train: Optional[Union[int, List[str]]] = None,
                  selection_val: Optional[Union[int, List[str]]] = None,
                  selection_test: Optional[Union[int, List[str]]] = None,
@@ -24,14 +24,15 @@ class RotNetDivaHisDBDataModuleCropped(AbstractDatamodule):
                  shuffle: bool = True, drop_last: bool = True):
         super().__init__()
 
-        analytics = get_analytics(input_path=Path(data_dir),
-                                  get_gt_data_paths_func=CroppedRotNet.get_gt_data_paths)
+        self.data_folder_name = data_folder_name
+        analytics_data = get_analytics_data(input_path=Path(data_dir), data_folder_name=self.data_folder_name,
+                                            get_gt_data_paths_func=CroppedRotNet.get_gt_data_paths)
 
-        self.mean = analytics['mean']
-        self.std = analytics['std']
+        self.mean = analytics_data['mean']
+        self.std = analytics_data['std']
         self.class_encodings = np.array(ROTATION_ANGLES)
         self.num_classes = len(self.class_encodings)
-        self.class_weights = np.ones(self.num_classes)
+        self.class_weights = np.array([1 / self.num_classes for _ in range(self.num_classes)])
 
         self.image_transform = OnlyImage(transforms.Compose([transforms.ToTensor(),
                                                              transforms.Normalize(mean=self.mean, std=self.std),
@@ -43,7 +44,6 @@ class RotNetDivaHisDBDataModuleCropped(AbstractDatamodule):
         self.shuffle = shuffle
         self.drop_last = drop_last
 
-        self.data_folder_name = data_folder_name
         self.data_dir = validate_path_for_self_supervised(data_dir=data_dir, data_folder_name=self.data_folder_name)
 
         self.selection_train = selection_train
@@ -113,7 +113,7 @@ class RotNetDivaHisDBDataModuleCropped(AbstractDatamodule):
     def _create_dataset_parameters(self, dataset_type: str = 'train'):
         is_test = dataset_type == 'test'
         return {'path': self.data_dir / dataset_type,
+                'data_folder_name': self.data_folder_name,
                 'image_transform': self.image_transform,
                 'classes': self.class_encodings,
                 'is_test': is_test}
-

src/datamodules/RotNet/utils/image_analytics.py
@@ -15,7 +15,7 @@ import torchvision.transforms as transforms
 from PIL import Image
 
 
-def get_analytics(input_path: Path, get_gt_data_paths_func, **kwargs):
+def get_analytics_data(input_path: Path, data_folder_name: str, get_gt_data_paths_func, **kwargs):
     """
     Parameters
     ----------
@@ -24,32 +24,41 @@ def get_analytics(input_path: Path, get_gt_data_paths_func, **kwargs):
     Returns
     -------
     """
-    analytics_file_path = input_path / 'analytics.json'
-    if analytics_file_path.exists():
-        with analytics_file_path.open(mode='r') as f:
-            analytics_dict = json.load(fp=f)
-    else:
-        train_path = input_path / 'train'
-        data_path_list = get_gt_data_paths_func(train_path)
-        file_names_data = np.asarray([str(item) for item in data_path_list])
-        mean, std = compute_mean_std(file_names=file_names_data, **kwargs)
+    expected_keys_data = ['mean', 'std']
 
-        # Measure weights for class balancing
-        logging.info(f'Measuring class weights')
-        # create a list with all gt file paths
-        analytics_dict = {'mean': mean.tolist(),
-                          'std': std.tolist()}
-        # save json
-        try:
-            with analytics_file_path.open(mode='w') as f:
-                json.dump(obj=analytics_dict, fp=f)
-        except IOError as e:
-            if e.errno == errno.EACCES:
-                print(f'WARNING: No permissions to write analytics file ({analytics_file_path})')
-            else:
-                raise
-    # returns the 'mean[RGB]', 'std[RGB]', 'class_frequencies_weights[num_classes]', 'class_encodings'
-    return analytics_dict
+    analytics_path_data = input_path / f'analytics.data.{data_folder_name}.json'
+
+    analytics_data = None
+
+    missing_analytics_data = True
+
+    if analytics_path_data.exists():
+        with analytics_path_data.open(mode='r') as f:
+            analytics_data = json.load(fp=f)
+        # check if analytics file is complete
+        if all(k in analytics_data for k in expected_keys_data):
+            missing_analytics_data = False
+
+    if missing_analytics_data:
+        train_path = input_path / 'train'
+        gt_data_path_list = get_gt_data_paths_func(train_path, data_folder_name=data_folder_name, gt_folder_name=None)
+        file_names_data = np.asarray([str(item) for item in gt_data_path_list])
+
+        if missing_analytics_data:
+            mean, std = compute_mean_std(file_names=file_names_data, **kwargs)
+            analytics_data = {'mean': mean.tolist(),
+                              'std': std.tolist()}
+            # save json
+            try:
+                with analytics_path_data.open(mode='w') as f:
+                    json.dump(obj=analytics_data, fp=f)
+            except IOError as e:
+                if e.errno == errno.EACCES:
+                    print(f'WARNING: No permissions to write analytics file ({analytics_path_data})')
+                else:
+                    raise
+
+    return analytics_data
 
 
 def compute_mean_std(file_names: List[Path], inmem=False, workers=4, **kwargs):
@@ -328,5 +337,4 @@ def _get_class_frequencies_weights_segmentation(gt_images, **kwargs):
 
 
 if __name__ == '__main__':
-    # print(get_analytics(input_path=Path('/netscratch/datasets/semantic_segmentation/datasets/CB55/'), inmem=True, workers=16))
-    print(get_analytics(input_path=Path('tests/dummy_data/dummy_dataset')))
+    print(get_analytics_data(input_path=Path('tests/dummy_data/dummy_dataset')))