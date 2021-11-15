7c791989b68d7bb420868067664b120072504a33
Lars Voegtlin <lars.voegtlin@unifr.ch>
2021-11-15 12:00:18 +0100

    :art: added dilation

--- src/models/backbones/resnet.py
+++ src/models/backbones/resnet.py
@@ -2,6 +2,7 @@
 Model definition adapted from: https://github.com/pytorch/vision/blob/master/torchvision/models/resnet.py
 """
 import math
+from typing import Optional, List, Union, Type
 
 import torch.nn as nn
 
@@ -23,8 +24,10 @@ def conv3x3(in_planes, out_planes, stride=1):
 class _BasicBlock(nn.Module):
     expansion = 1
 
-    def __init__(self, inplanes, planes, stride=1, downsample=None):
+    def __init__(self, inplanes, planes, stride=1, downsample=None, dilation: int = 1):
         super(_BasicBlock, self).__init__()
+        if dilation > 1:
+            raise NotImplementedError("Dilation > 1 not implemented in BasicBlock")
         self.conv1 = conv3x3(inplanes, planes, stride)
         self.bn1 = nn.BatchNorm2d(planes)
         self.relu = nn.ReLU(inplace=True)
@@ -55,15 +58,15 @@ class _BasicBlock(nn.Module):
 class _Bottleneck(nn.Module):
     expansion = 4
 
-    def __init__(self, inplanes, planes, stride=1, downsample=None):
+    def __init__(self, inplanes, planes, stride=1, downsample=None, dilation=False):
         super(_Bottleneck, self).__init__()
         self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=(1, 1), bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=(3, 3), stride=stride,
-                               padding=1, bias=False)
+                               padding=1, bias=False, dilation=dilation)
         self.bn2 = nn.BatchNorm2d(planes)
-        self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=(1, 1), bias=False)
-        self.bn3 = nn.BatchNorm2d(planes * 4)
+        self.conv3 = nn.Conv2d(planes, planes * self.expansion, kernel_size=(1, 1), bias=False)
+        self.bn3 = nn.BatchNorm2d(planes * self.expansion)
         self.relu = nn.ReLU(inplace=True)
         self.downsample = downsample
         self.stride = stride
@@ -93,9 +96,17 @@ class _Bottleneck(nn.Module):
 
 class ResNet(nn.Module):
 
-    def __init__(self, block, layers, **kwargs):
-        self.inplanes = 64
+    def __init__(self, block: Type[Union[_BasicBlock, _Bottleneck]], layers: List[int],
+                 replace_stride_with_dilation: Optional[List[bool]] = None, **kwargs):
         super(ResNet, self).__init__()
+        self.inplanes = 64
+        self.dilation = 1
+
+        if replace_stride_with_dilation is None:
+            replace_stride_with_dilation = [False, False, False]
+        if len(replace_stride_with_dilation) != 3:
+            raise ValueError(
+                f"replace_stride_with_dilation should be None or a 3-tuple, got {replace_stride_with_dilation}")
 
         self.conv1 = nn.Conv2d(3, 64, kernel_size=(7, 7), stride=2, padding=3,
                                bias=False)
@@ -103,9 +114,9 @@ class ResNet(nn.Module):
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         self.layer1 = self._make_layer(block, 64, layers[0])
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2, dilate=replace_stride_with_dilation[0])
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2, dilate=replace_stride_with_dilation[1])
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2, dilate=replace_stride_with_dilation[2])
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
@@ -115,8 +126,13 @@ class ResNet(nn.Module):
                 m.weight.data.fill_(1)
                 m.bias.data.zero_()
 
-    def _make_layer(self, block, planes, blocks, stride=1):
+    def _make_layer(self, block: Type[Union[_BasicBlock, _Bottleneck]], planes: int, blocks: int, stride: int = 1,
+                    dilate: bool = False):
         downsample = None
+        previous_dilation = self.dilation
+        if dilate:
+            self.dilation *= stride
+            stride = 1
         if stride != 1 or self.inplanes != planes * block.expansion:
             downsample = nn.Sequential(
                 nn.Conv2d(self.inplanes, planes * block.expansion,
@@ -125,7 +141,8 @@ class ResNet(nn.Module):
             )
 
         layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample))
+        layers.append(block(inplanes=self.inplanes, planes=planes, stride=stride, downsample=downsample,
+                            dilation=previous_dilation))
         self.inplanes = planes * block.expansion
         for i in range(1, blocks):
             layers.append(block(self.inplanes, planes))
