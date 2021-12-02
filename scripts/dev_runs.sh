ca126609d5384d9a603f173028597f11cc48de83
Paul M <paul.maergner@gmail.com>
2021-12-02 17:19:04 +0100

    :wrench: changed default crossentropyloss to not use class weights.

---
+++ scripts/dev_runs.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+python run.py experiment=development_baby_unet_cb55_10.yaml
+python run.py experiment=development_baby_unet_rgb_data.yaml
+python run.py experiment=dev_rgb_full_page.yaml
+python run.py experiment=dev_rolf_format.yaml
