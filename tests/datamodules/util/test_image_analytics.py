8d768b405b8d1d118b71ab7c89af4da25abe242d
Lars Voegtlin <lars.voegtlin@unifr.ch>
2021-12-03 14:08:58 +0100

    :white_check_mark: replaced with isclose

--- tests/datamodules/util/test_image_analytics.py
+++ tests/datamodules/util/test_image_analytics.py
@@ -8,16 +8,16 @@ def test_compute_mean_std_inmem(data_dir_cropped):
     path_to_files = data_dir_cropped / 'train' / 'data' / 'e-codices_fmb-cb-0055_0098v_max'
     path_list = list(path_to_files.iterdir())
     mean, std = compute_mean_std(file_names=path_list, inmem=True, workers=1)
-    assert list(mean) == [0.7050454974582425, 0.6503181590413943, 0.5567698583877996]
-    assert list(std) == [0.31040608596198827, 0.30533118388840325, 0.28919611393432737]
+    assert np.isclose(mean, [0.7050454974582425, 0.6503181590413943, 0.5567698583877996]).any()
+    assert np.isclose(std, [0.31040608596198827, 0.30533118388840325, 0.28919611393432737]).any()
 
 
 def test_compute_mean_std_not_inmem(data_dir_cropped):
     path_to_files = data_dir_cropped / 'train' / 'data' / 'e-codices_fmb-cb-0055_0098v_max'
     path_list = list(path_to_files.iterdir())
     mean, std = compute_mean_std(file_names=path_list, inmem=False, workers=1)
-    assert list(mean) == [0.7050454974582426, 0.6503181590413943, 0.5567698583877997]
-    assert list(std) == [0.3104060859619883, 0.30533118388840325, 0.28919611393432726]
+    assert np.isclose(mean, [0.7050454974582426, 0.6503181590413943, 0.5567698583877997]).any()
+    assert np.isclose(std, [0.3104060859619883, 0.30533118388840325, 0.28919611393432726]).any()
 
 
 def test__return_mean(data_dir):
