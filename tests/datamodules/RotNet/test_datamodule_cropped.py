65dffd49c3181dab815d4b1b858ef52ca9be487b
Lars Voegtlin <lars.voegtlin@unifr.ch>
2021-12-06 17:37:07 +0100

    :recycle: some refactoring

--- tests/datamodules/RotNet/test_datamodule_cropped.py
+++ tests/datamodules/RotNet/test_datamodule_cropped.py
@@ -10,30 +10,30 @@ NUM_WORKERS = 4
 DATA_FOLDER_NAME = 'data'
 
 @pytest.fixture
-def data_module_cropped(data_dir_cropped):
+def data_module_cropped_rotnet(data_dir_cropped):
     OmegaConf.clear_resolvers()
     return RotNetDivaHisDBDataModuleCropped(data_dir=data_dir_cropped,
                                             data_folder_name=DATA_FOLDER_NAME,
                                             num_workers=NUM_WORKERS)
 
 
-def test_init_datamodule(data_module_cropped):
-    assert data_module_cropped.batch_size == 8
-    assert data_module_cropped.num_workers == NUM_WORKERS
-    assert data_module_cropped.dims == (3, 256, 256)
-    assert data_module_cropped.num_classes == 4
-    assert np.array_equal(data_module_cropped.class_encodings, [0, 90, 180, 270])
-    assert torch.equal(data_module_cropped.class_weights, torch.tensor([.25, .25, .25, .25]))
-    assert data_module_cropped.mean == [0.7050454974582426, 0.6503181590413943, 0.5567698583877997]
-    assert data_module_cropped.std == [0.3104060859619883, 0.3053311838884032, 0.28919611393432726]
+def test_init_datamodule(data_module_cropped_rotnet):
+    assert data_module_cropped_rotnet.batch_size == 8
+    assert data_module_cropped_rotnet.num_workers == NUM_WORKERS
+    assert data_module_cropped_rotnet.dims == (3, 256, 256)
+    assert data_module_cropped_rotnet.num_classes == 4
+    assert np.array_equal(data_module_cropped_rotnet.class_encodings, [0, 90, 180, 270])
+    assert torch.equal(data_module_cropped_rotnet.class_weights, torch.tensor([.25, .25, .25, .25]))
+    assert data_module_cropped_rotnet.mean == [0.7050454974582426, 0.6503181590413943, 0.5567698583877997]
+    assert data_module_cropped_rotnet.std == [0.3104060859619883, 0.3053311838884032, 0.28919611393432726]
     with pytest.raises(AttributeError):
-        getattr(data_module_cropped, 'train')
-        getattr(data_module_cropped, 'val')
-        getattr(data_module_cropped, 'test')
+        getattr(data_module_cropped_rotnet, 'train')
+        getattr(data_module_cropped_rotnet, 'val')
+        getattr(data_module_cropped_rotnet, 'test')
 
 
-def test__create_dataset_parameters_cropped(data_module_cropped):
-    parameters = data_module_cropped._create_dataset_parameters()
+def test__create_dataset_parameters_cropped(data_module_cropped_rotnet):
+    parameters = data_module_cropped_rotnet._create_dataset_parameters()
     assert 'train' in str(parameters['path'])
     assert not parameters['is_test']
     assert np.array_equal(parameters['classes'], np.array([0, 90, 180, 270], dtype=np.int64))
