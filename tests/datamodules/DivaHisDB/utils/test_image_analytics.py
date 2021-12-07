352f847ab52ed666830e2118ff518b8fe054a7a7
Lars Voegtlin <lars.voegtlin@unifr.ch>
2021-12-07 15:29:48 +0100

    :white_check_mark: added rotnet datamodule tests and refined others

--- tests/datamodules/DivaHisDB/utils/test_image_analytics.py
+++ tests/datamodules/DivaHisDB/utils/test_image_analytics.py
@@ -3,7 +3,7 @@ import json
 import numpy as np
 
 from src.datamodules.DivaHisDB.datasets.cropped_dataset import CroppedHisDBDataset
-from src.datamodules.DivaHisDB.utils.image_analytics import get_analytics
+from src.datamodules.DivaHisDB.utils.image_analytics import get_analytics, get_class_weights
 from tests.test_data.dummy_data_hisdb.dummy_data import data_dir_cropped
 
 TEST_JSON_DATA = {'mean': [0.7050454974582426, 0.6503181590413943, 0.5567698583877997],
@@ -43,3 +43,8 @@ def test_get_analytics_load_from_file(data_dir_cropped):
     assert analytics_path.exists()
 
     test_get_analytics_no_file(data_dir_cropped=data_dir_cropped)
+
+
+def test_get_class_weights(data_dir_cropped):
+    weights = get_class_weights(input_folder=data_dir_cropped)
+    assert np.array_equal(weights, [0.2857142857142857, 0.35714285714285715, 0.35714285714285715])
--- tests/datamodules/RotNet/test_datamodule_cropped.py
+++ tests/datamodules/RotNet/test_datamodule_cropped.py
@@ -2,12 +2,14 @@ import numpy as np
 import pytest
 import torch
 from omegaconf import OmegaConf
+from pytorch_lightning import Trainer
 
 from src.datamodules.RotNet.datamodule_cropped import RotNetDivaHisDBDataModuleCropped
 from tests.test_data.dummy_data_hisdb.dummy_data import data_dir_cropped
 
 NUM_WORKERS = 4
 DATA_FOLDER_NAME = 'data'
+
 
 @pytest.fixture
 def data_module_cropped_rotnet(data_dir_cropped):
@@ -37,3 +39,31 @@ def test__create_dataset_parameters_cropped(data_module_cropped_rotnet):
     assert 'train' in str(parameters['path'])
     assert not parameters['is_test']
     assert np.array_equal(parameters['classes'], np.array([0, 90, 180, 270], dtype=np.int64))
+
+
+def test__check_min_num_samples(data_module_cropped_rotnet, monkeypatch):
+    trainer = Trainer()
+    monkeypatch.setattr(data_module_cropped_rotnet, 'trainer', trainer)
+    data_module_cropped_rotnet._check_min_num_samples(num_samples=10, data_split='train', drop_last=True)
+
+
+def test__check_min_num_samples_drop_error(data_module_cropped_rotnet, monkeypatch):
+    trainer = Trainer()
+    monkeypatch.setattr(data_module_cropped_rotnet, 'batch_size', 20)
+    monkeypatch.setattr(data_module_cropped_rotnet, 'trainer', trainer)
+    with pytest.raises(ValueError):
+        data_module_cropped_rotnet._check_min_num_samples(num_samples=10, data_split='train', drop_last=True)
+
+
+def test__check_min_num_samples_no_drop_error(data_module_cropped_rotnet, monkeypatch, caplog):
+    num_samples = 10
+    data_split = 'train'
+    batch_size = 20
+    trainer = Trainer()
+    monkeypatch.setattr(data_module_cropped_rotnet, 'batch_size', batch_size)
+    monkeypatch.setattr(data_module_cropped_rotnet, 'trainer', trainer)
+    data_module_cropped_rotnet._check_min_num_samples(num_samples=num_samples, data_split=data_split, drop_last=False)
+    assert f'#samples ({num_samples}) in "{data_split}" smaller than ' \
+           f'#processes (1) times batch size ({batch_size}). ' \
+           f'This works due to drop_last=False, however samples might occur multiple times. ' \
+           f'Check if this behavior is intended!' in caplog.text
