0605d80c5c4649561d4e0fd5f4c8d9e4d30fb7ae
Paul M <paul.maergner@gmail.com>
2021-12-10 18:03:22 +0100

    :wrench: modifying experiment script for finetune

--- scripts/experiments/cb55_finetune_rotnet_resnet50.sh
+++ scripts/experiments/cb55_finetune_rotnet_resnet50.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
-weights="/netscratch/experiments_lars_paul/lars/experiments/rotnet_cb55_full_resnet50/2021-12-07/18-33-43/checkpoints/epoch=39/backbone.pth
-         /netscratch/experiments_lars_paul/lars/experiments/rotnet_cb55_full_resnet50/2021-12-07/22-33-54/checkpoints/epoch=43/backbone.pth
-         /netscratch/experiments_lars_paul/lars/experiments/rotnet_cb55_full_resnet50/2021-12-08/02-33-35/checkpoints/epoch=19/backbone.pth"
+weights="/netscratch/experiments_lars_paul/lars/experiments/rotnet_cb55_full_resnet50/2021-12-07/18-33-43/checkpoints/epoch\=39/backbone.pth
+         /netscratch/experiments_lars_paul/lars/experiments/rotnet_cb55_full_resnet50/2021-12-07/22-33-54/checkpoints/epoch\=43/backbone.pth
+         /netscratch/experiments_lars_paul/lars/experiments/rotnet_cb55_full_resnet50/2021-12-08/02-33-35/checkpoints/epoch\=19/backbone.pth"
 
 seed="255827881"
 
 for weight in ${weights}
 do
-  params="+seed=\"${seed}\" +model.backbone.path_to_weights=\"${weight}\""
+  params="+seed=${seed} +model.backbone.path_to_weights=\"${weight}\""
   python run.py experiment=cb55_select_train1_val1_run_resnet50_finetune_rotnet ${params}
   python run.py experiment=cb55_select_train15_run_resnet50_finetune_rotnet     ${params}
   python run.py experiment=cb55_full_run_resnet50_finetune_rotnet               ${params}
@@ -17,7 +17,7 @@ done
 
 for weight in ${weights}
 do
-  params="+seed=\"${seed}\" +model.backbone.path_to_weights=\"${weight}\" logger.wandb.group=\"finetune-freeze\""
+  params="+seed=${seed} +model.backbone.path_to_weights=\"${weight}\" logger.wandb.group=finetune-freeze"
   python run.py experiment=cb55_select_train1_val1_run_resnet50_finetune_rotnet ${params} +model.backbone.freeze="True"
   python run.py experiment=cb55_select_train15_run_resnet50_finetune_rotnet     ${params} +model.backbone.freeze="True"
   python run.py experiment=cb55_full_run_resnet50_finetune_rotnet               ${params} +model.backbone.freeze="True"
