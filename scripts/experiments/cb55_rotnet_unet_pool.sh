c9b5916d94ba9999bf76866ed45a11d8807c2fe5
Paul M <paul.maergner@gmail.com>
2021-12-15 15:32:05 +0100

    :wrench: rotnet unet experiments

---
+++ scripts/experiments/cb55_rotnet_unet_pool.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -e
+
+seeds="3323771338
+       1544775463
+       392423028"
+
+params="experiment=rotnet_unet_cb55_full_pool"
+
+for seed in ${seeds}
+do
+  python run.py ${params} +seed="${seed}"
+done
+
+
+params="experiment=rotnet_unet_cb55_full_convpool"
+
+for seed in ${seeds}
+do
+  python run.py ${params} +seed="${seed}"
+done
