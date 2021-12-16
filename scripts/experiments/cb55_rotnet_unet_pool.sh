faed5ccaf98557f0e16de38931b3b5c341934cb8
Lars Voegtlin <lars.voegtlin@unifr.ch>
2021-12-16 17:28:30 +0100

    :wrench: removed the convpool from the poll script

--- scripts/experiments/cb55_rotnet_unet_pool.sh
+++ scripts/experiments/cb55_rotnet_unet_pool.sh
@@ -1,22 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-seeds="1544775463
-       392423028"
-
-params="experiment=rotnet_unet_cb55_full_pool"
-
-for seed in ${seeds}
-do
-  python run.py ${params} +seed="${seed}"
-done
-
-
 seeds="3323771338
        1544775463
        392423028"
 
-params="experiment=rotnet_unet_cb55_full_convpool"
+params="experiment=rotnet_unet_cb55_full_pool datamodule.batch_size=100"
 
 for seed in ${seeds}
 do
