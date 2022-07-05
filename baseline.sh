3ef8e827f1f2591a3ff51523a96cdff57b05d262
Vinay Pondenkandath <vinaychandranp@gmail.com>
2022-07-05 09:28:50 +0200

    updated launch scripts

--- baseline.sh
+++ baseline.sh
@@ -1,5 +1,5 @@
-CUDA_VISIBLE_DEVICES=0 python run.py experiment=cb55_full_run_deeplab_vinay name=0 &
-CUDA_VISIBLE_DEVICES=1 python run.py experiment=cb55_full_run_deeplab_vinay name=1 &
-CUDA_VISIBLE_DEVICES=2 python run.py experiment=cb55_full_run_deeplab_vinay name=2 &
-CUDA_VISIBLE_DEVICES=3 python run.py experiment=cb55_full_run_deeplab_vinay name=3 &
-CUDA_VISIBLE_DEVICES=4 python run.py experiment=cb55_full_run_deeplab_vinay name=4 & 
+CUDA_VISIBLE_DEVICES=0 python run.py experiment=cb55_full_run_deeplab_vinay name=a &
+CUDA_VISIBLE_DEVICES=1 python run.py experiment=cb55_full_run_deeplab_vinay name=b &
+CUDA_VISIBLE_DEVICES=2 python run.py experiment=cb55_full_run_deeplab_vinay name=c &
+CUDA_VISIBLE_DEVICES=3 python run.py experiment=cb55_full_run_deeplab_vinay name=d &
+CUDA_VISIBLE_DEVICES=4 python run.py experiment=cb55_full_run_deeplab_vinay name=e & 
--- pretrained.sh
+++ pretrained.sh
@@ -1,5 +1,5 @@
-CUDA_VISIBLE_DEVICES=5 python run.py experiment=cb55_full_run_deeplab_pt_vinay name=0 &
-CUDA_VISIBLE_DEVICES=6 python run.py experiment=cb55_full_run_deeplab_pt_vinay name=1 &
-CUDA_VISIBLE_DEVICES=7 python run.py experiment=cb55_full_run_deeplab_pt_vinay name=2 &
-CUDA_VISIBLE_DEVICES=5 python run.py experiment=cb55_full_run_deeplab_pt_vinay name=3 &
-CUDA_VISIBLE_DEVICES=6 python run.py experiment=cb55_full_run_deeplab_pt_vinay name=4 & 
+CUDA_VISIBLE_DEVICES=5 python run.py experiment=cb55_full_run_deeplab_pt_vinay name=a &
+CUDA_VISIBLE_DEVICES=6 python run.py experiment=cb55_full_run_deeplab_pt_vinay name=b &
+CUDA_VISIBLE_DEVICES=7 python run.py experiment=cb55_full_run_deeplab_pt_vinay name=c &
+CUDA_VISIBLE_DEVICES=5 python run.py experiment=cb55_full_run_deeplab_pt_vinay name=d &
+CUDA_VISIBLE_DEVICES=6 python run.py experiment=cb55_full_run_deeplab_pt_vinay name=e & 
