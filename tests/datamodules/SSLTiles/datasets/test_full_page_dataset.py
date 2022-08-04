2f05a43473745cb431f86205ca80a1f31caa3832
Lars Voegtlin <lars.voegtlin@unifr.ch>
2022-08-04 14:45:39 +0200

    :white_check_mark: _cut_image_in_tiles_and_put_together tested

--- tests/datamodules/SSLTiles/datasets/test_full_page_dataset.py
+++ tests/datamodules/SSLTiles/datasets/test_full_page_dataset.py
@@ -1,10 +1,13 @@
 import numpy as np
 import pytest
+from PIL import ImageChops
 from torchvision.datasets.folder import pil_loader
 
 from src.datamodules.SSLTiles.datasets.full_page_dataset import DatasetSSLTiles
 from src.datamodules.utils.misc import ImageDimensions
 from tests.test_data.dummy_data_histdb_new.dummy_data import data_dir
+from tests.test_data.result_data_ssltiles.result_data import result_img_2_3_horizontal, result_img_2_3_vertical, \
+    result_img_2_3_horizontal_vertical
 
 DATA_FOLDER_NAME = 'data'
 
@@ -26,11 +29,46 @@ def test_get_img_gt_path_list(data_dir):
     assert img_gt_path_list[1].name == 'fmb-cb-55-005v_2.png'
 
 
-def test__cut_image_in_tiles_and_put_together(dataset_train, data_dir, monkeypatch):
+def test__cut_image_in_tiles_and_put_together_2_3_vertical(dataset_train, data_dir, result_img_2_3_vertical,
+                                                           monkeypatch):
+    img = data_dir / 'train' / 'data' / 'fmb-cb-55-005v.png'
+    img = np.array(pil_loader(img))
+    monkeypatch.setattr(dataset_train, 'vertical_shuffle', True)
+    monkeypatch.setattr(dataset_train, 'horizontal_shuffle', False)
+    np.random.seed(42)
+    tile_img, gt = dataset_train._cut_image_in_tiles_and_put_together(img)
+    assert gt.shape == (3, 2)
+    assert tile_img.width == 960
+    assert tile_img.height == 1344
+    assert np.array_equal(gt, np.array([[0, 3], [2, 5], [4, 1]]))
+    assert ImageChops.difference(tile_img, result_img_2_3_vertical).getbbox() is None
+
+
+def test__cut_image_in_tiles_and_put_together_2_3_horizontal(dataset_train, data_dir, result_img_2_3_horizontal,
+                                                             monkeypatch):
     img = data_dir / 'train' / 'data' / 'fmb-cb-55-005v.png'
     img = np.array(pil_loader(img))
     monkeypatch.setattr(dataset_train, 'vertical_shuffle', False)
     monkeypatch.setattr(dataset_train, 'horizontal_shuffle', True)
     np.random.seed(42)
     tile_img, gt = dataset_train._cut_image_in_tiles_and_put_together(img)
-    assert False
+    assert gt.shape == (3, 2)
+    assert tile_img.width == 960
+    assert tile_img.height == 1344
+    assert np.array_equal(gt, np.array([[1, 0], [2, 3], [5, 4]]))
+    assert ImageChops.difference(tile_img, result_img_2_3_horizontal).getbbox() is None
+
+
+def test__cut_image_in_tiles_and_put_together_2_3_horizontal_vertical(dataset_train, data_dir,
+                                                                      result_img_2_3_horizontal_vertical, monkeypatch):
+    img = data_dir / 'train' / 'data' / 'fmb-cb-55-005v.png'
+    img = np.array(pil_loader(img))
+    monkeypatch.setattr(dataset_train, 'vertical_shuffle', True)
+    monkeypatch.setattr(dataset_train, 'horizontal_shuffle', True)
+    np.random.seed(42)
+    tile_img, gt = dataset_train._cut_image_in_tiles_and_put_together(img)
+    assert gt.shape == (3, 2)
+    assert tile_img.width == 960
+    assert tile_img.height == 1344
+    assert np.array_equal(gt, np.array([[2, 3], [1, 4], [5, 0]]))
+    assert ImageChops.difference(tile_img, result_img_2_3_horizontal_vertical).getbbox() is None
