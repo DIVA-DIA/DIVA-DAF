ba70d6909d323c80f27a680d10791a1076b7cf1f
Lars Voegtlin <lars.voegtlin@unifr.ch>
2022-08-19 11:26:16 +0200

    :white_check_mark: added tests for the new prebuilt datamodule (no workflow test yet)

--- tests/test_data/dummy_data_histdb_new/dummy_data.py
+++ tests/test_data/dummy_data_histdb_new/dummy_data.py
@@ -18,3 +18,19 @@ def data_dir(tmp_path):
         dir_util.copy_tree(test_dir, str(tmp_path))
 
     return tmp_path
+
+
+@pytest.fixture
+def data_dir_classification(tmp_path):
+    """
+    Moves the test data into the tmp path of the testing environment.
+    :param tmp_path:
+    :return:
+    """
+    filename = __file__
+    test_dir = os.path.join(os.path.dirname(filename), 'dummy_dataset_classification')
+
+    if os.path.isdir(test_dir):
+        dir_util.copy_tree(test_dir, str(tmp_path))
+
+    return tmp_path
