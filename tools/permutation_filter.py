6b0a143d90ad5a544c56b867a5803c69451fdf65
Lars Voegtlin <lars.voegtlin@unifr.ch>
2022-08-24 16:23:05 +0200

    :art: refactored to method

--- tools/permutation_filter.py
+++ tools/permutation_filter.py
@@ -13,18 +13,13 @@ def get_perms_with_n_fixed_positions(permutations: List, classes: List[int], mat
     return np.array([p for p in perms if np.sum(p != cls) == matches])
 
 
-if __name__ == '__main__':
-    positions_w_do_not_change = 3
-    classes = list(range(6))
+def filter_by_permutations(classes: List[int], matches: int, root_path: Path) -> None:
     all_perms = list(itertools.permutations(classes))
-    used_perms = get_perms_with_n_fixed_positions(all_perms, classes, positions_w_do_not_change)
-
-    root_path = Path('/net/research-hisdoc/datasets/self-supervised/CB55/tiles_960_1344_embeded/all_files')
+    used_perms = get_perms_with_n_fixed_positions(all_perms, classes, matches)
     perm_mapping_path = root_path / 'permutations.json'
-    output_path = root_path.parent / f'{positions_w_do_not_change}_fixed_positions'
+    output_path = root_path.parent / f'{matches}_fixed_positions'
     output_path_info = output_path / 'permutations.json'
     output_path.mkdir(exist_ok=True)
-
     perm_mappings = np.asarray(json.load(perm_mapping_path.open()))
     permutation_classes = [np.where(np.all(perm_mappings == tuple(p), axis=1))[0][0] for p in used_perms if
                            # checks if there is a match in the permutation mapping
@@ -37,5 +32,10 @@ if __name__ == '__main__':
             if (class_folder / file_path.name).exists():
                 continue
             (class_folder / file_path.name).symlink_to(file_path)
-
     json.dump([int(i) for i in permutation_classes], output_path_info.open('w'))
+
+
+if __name__ == '__main__':
+    positions_w_do_not_change = 3
+    filter_by_permutations(classes=list(range(6)), matches=positions_w_do_not_change, root_path=Path(
+        '/net/research-hisdoc/datasets/self-supervised/CB55/tiles_960_1344_embeded/all_files'))
