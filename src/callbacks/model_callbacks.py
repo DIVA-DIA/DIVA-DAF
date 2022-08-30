5a7cf95e1af648ac7a0150c63bd06e8eb7e392fa
Lars Voegtlin <lars.voegtlin@unifr.ch>
2022-08-30 17:34:13 +0200

    :bug: adapted to new model checkpoint algo

--- src/callbacks/model_callbacks.py
+++ src/callbacks/model_callbacks.py
@@ -26,25 +26,13 @@ class SaveModelStateDictAndTaskCheckpoint(ModelCheckpoint):
         self.header_filename = header_filename
         self.CHECKPOINT_NAME_LAST = 'task_last'
 
-    @rank_zero_only
-    def _del_model(self, trainer: pl.Trainer, filepath: str) -> None:
-        if trainer.should_rank_save_checkpoint and self._fs.exists(filepath):
-            parent_dir = self._fs._parent(filepath)
-            # delete all files in directory
-            for path in self._fs.ls(parent_dir):
-                if self._fs.exists(path):
-                    self._fs.rm(path)
-            # delete directory
-            self._fs.rmdir(parent_dir)
-            log.debug(f"Removed checkpoint: {filepath}")
-
-    def _save_model(self, trainer: pl.Trainer, filepath: str) -> None:
-        super()._save_model(trainer=trainer, filepath=filepath)
+    def _save_checkpoint(self, trainer: "pl.Trainer", filepath: str) -> None:
+        super()._save_checkpoint(trainer=trainer, filepath=filepath)
         if not trainer.is_global_zero:
             return
 
         model = trainer.lightning_module.model
-        metric_candidates = self._monitor_candidates(trainer, epoch=trainer.current_epoch, step=trainer.global_step)
+        metric_candidates = self._monitor_candidates(trainer)
         # check if it is a last save or not
         if 'last' not in filepath:
             # fixed pathing problem
@@ -52,12 +40,27 @@ class SaveModelStateDictAndTaskCheckpoint(ModelCheckpoint):
                                                                     metrics=metric_candidates)
             format_header_filename = self._format_checkpoint_name(filename=self.header_filename,
                                                                   metrics=metric_candidates)
+            if trainer.current_epoch > 0:
+                # remove the last checkpoint folder
+                filepath_old = os.path.join(self.dirpath, f"epoch={trainer.current_epoch - 1}")
+                self._del_old_folder(filepath_old)
         else:
             format_backbone_filename = self.backbone_filename.split('/')[-1] + '_last'
             format_header_filename = self.header_filename.split('/')[-1] + '_last'
 
         torch.save(model.backbone.state_dict(), os.path.join(self.dirpath, format_backbone_filename + '.pth'))
         torch.save(model.header.state_dict(), os.path.join(self.dirpath, format_header_filename + '.pth'))
+
+    @rank_zero_only
+    def _del_old_folder(self, filepath: str) -> None:
+        if self._fs.exists(filepath):
+            # delete all files in directory
+            for path in self._fs.ls(filepath):
+                if self._fs.exists(path):
+                    self._fs.rm(path)
+            # delete directory
+            self._fs.rmdir(filepath)
+            log.debug(f"Removed checkpoint: {filepath}")
 
 
 class CheckBackboneHeaderCompatibility(Callback):
