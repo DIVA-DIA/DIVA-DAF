fc6bb24ff9abea56bb715c7d17ecdfcfe1d55b85
Lars Voegtlin <lars.voegtlin@unifr.ch>
2022-11-17 15:35:42 +0100

    :recycle: all ImageDims form same source

--- src/datamodules/RGB/datamodule.py
+++ src/datamodules/RGB/datamodule.py
@@ -5,12 +5,12 @@ import torch
 from torch.utils.data import DataLoader
 from torchvision import transforms
 
-from src.datamodules.RGB.datasets.full_page_dataset import DatasetRGB, ImageDimensions
+from src.datamodules.RGB.datasets.full_page_dataset import DatasetRGB
 from src.datamodules.RGB.utils.image_analytics import get_analytics
 from src.datamodules.RGB.utils.single_transform import IntegerEncoding
 from src.datamodules.base_datamodule import AbstractDatamodule
 from src.datamodules.utils.dataset_predict import DatasetPredict
-from src.datamodules.utils.misc import validate_path_for_segmentation
+from src.datamodules.utils.misc import validate_path_for_segmentation, ImageDimensions
 from src.datamodules.utils.wrapper_transforms import OnlyImage, OnlyTarget
 from src.utils import utils
 
