bc3571d04ddf403e625bfba3362381257e7200ae
Lars Voegtlin <lars.voegtlin@unifr.ch>
2022-11-22 16:00:08 +0100

    :see_no_evil: added standart ouput path of the experiments

--- tools/summary_models.py
+++ tools/summary_models.py
@@ -1,17 +1,18 @@
 import torch
 from torch.nn import Identity
-from torchsummary import summary
+from torchinfo import summary
 from torchvision.models.segmentation import fcn_resnet50
 
 from src.models.backbone_header_model import BackboneHeaderModel
 from src.models.backbones import ResNet50
-from src.models.backbones.divanet import DivaNet
 from src.models.backbones.unet import UNet
 from src.models.headers.fully_connected import ResNetHeader
 from src.models.headers.fully_convolution import ResNetFCNHead
 
 if __name__ == '__main__':
     # model = fcn_resnet50(num_classes=4, weights_backbone=None)
-    model = BackboneHeaderModel(backbone=UNet(num_classes=4), header=Identity())
+    model = UNet(num_classes=4)
     model.to(torch.device('cuda'))
-    summary(model, (3, 244, 244))
+    model.eval()
+    model.half()  # to FP16
+    summary(model, (1, 3, 1152, 1728), device='cuda', dtypes=[torch.float16])
