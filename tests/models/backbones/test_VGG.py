211e6781afe921f3248e837e6e222973cc8df5bf
Lars Voegtlin <lars.voegtlin@unifr.ch>
2023-05-15 11:05:05 +0200

    :white_check_mark: added more tests

--- tests/models/backbones/test_VGG.py
+++ tests/models/backbones/test_VGG.py
@@ -1,4 +1,4 @@
-from src.models.backbones.VGG import vgg11
+from src.models.backbones.VGG import vgg11, vgg19_bn, vgg19, vgg16_bn, vgg16, vgg13_bn, vgg13, vgg11_bn
 import torch
 
 
@@ -9,3 +9,58 @@ def test_vgg11():
     assert output_tensor.shape == torch.Size([1, 5])
     assert not output_tensor.isnan().any()
 
+
+def test_vgg11_bn():
+    model = vgg11_bn(num_classes=5)
+    model.eval()
+    output_tensor = model(torch.rand(1, 3, 224, 224))
+    assert output_tensor.shape == torch.Size([1, 5])
+    assert not output_tensor.isnan().any()
+
+
+def test_vgg13():
+    model = vgg13(num_classes=5)
+    model.eval()
+    output_tensor = model(torch.rand(1, 3, 224, 224))
+    assert output_tensor.shape == torch.Size([1, 5])
+    assert not output_tensor.isnan().any()
+
+
+def test_vgg13_bn():
+    model = vgg13_bn(num_classes=5)
+    model.eval()
+    output_tensor = model(torch.rand(1, 3, 224, 224))
+    assert output_tensor.shape == torch.Size([1, 5])
+    assert not output_tensor.isnan().any()
+
+
+def test_vgg16():
+    model = vgg16(num_classes=5)
+    model.eval()
+    output_tensor = model(torch.rand(1, 3, 224, 224))
+    assert output_tensor.shape == torch.Size([1, 5])
+    assert not output_tensor.isnan().any()
+
+
+def test_vgg16_bn():
+    model = vgg16_bn(num_classes=5)
+    model.eval()
+    output_tensor = model(torch.rand(1, 3, 224, 224))
+    assert output_tensor.shape == torch.Size([1, 5])
+    assert not output_tensor.isnan().any()
+
+
+def test_vgg19():
+    model = vgg19(num_classes=5)
+    model.eval()
+    output_tensor = model(torch.rand(1, 3, 224, 224))
+    assert output_tensor.shape == torch.Size([1, 5])
+    assert not output_tensor.isnan().any()
+
+
+def test_vgg19_bn():
+    model = vgg19_bn(num_classes=5)
+    model.eval()
+    output_tensor = model(torch.rand(1, 3, 224, 224))
+    assert output_tensor.shape == torch.Size([1, 5])
+    assert not output_tensor.isnan().any()
