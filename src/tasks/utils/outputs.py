298fabec45792b0de3a916ce7ca554b2a0b81ce3
Lars Voegtlin <lars.voegtlin@unifr.ch>
2024-02-13 10:40:52 +0100

    :books: documentation for task utils

--- src/tasks/utils/outputs.py
+++ src/tasks/utils/outputs.py
@@ -4,6 +4,9 @@ from pytorch_lightning.utilities import LightningEnum
 
 
 class OutputKeys(LightningEnum):
+    """
+    Enum class for the keys of the output dictionary.
+    """
     PREDICTION = 'pred'
     TARGET = 'target'
     LOG = 'logs'
@@ -14,4 +17,14 @@ class OutputKeys(LightningEnum):
 
 
 def reduce_dict(input_dict: Dict, key_list: List) -> Dict:
+    """
+    Reduce the input dictionary to only contain the keys in the key_list.
+
+    :param input_dict: The dictionary to reduce
+    :type input_dict: Dict
+    :param key_list: List of keys to keep
+    :type key_list: List
+    :return: The dictionary input_dict with only the keys in the key_list
+    :rtype: Dict
+    """
     return {key: input_dict[key] for key in key_list if key in input_dict}
--- src/tasks/utils/task_utils.py
+++ src/tasks/utils/task_utils.py
@@ -3,10 +3,26 @@ from typing import Callable, Mapping, Sequence, Dict, Union
 
 # inspired by https://github.com/PyTorchLightning/lightning-flash/blob/2ec52e633bb3679f50dd7e30526885a4547e1851/flash/core/utilities/apply_func.py
 def get_callable_name(fn_or_class: Union[Callable, Sequence, object]) -> str:
+    """
+    Get the name of a callable or class.
+
+    :param fn_or_class: Callable, class or sequence we want the name of
+    :type fn_or_class: Union[Callable, Sequence, object]
+    :return: the name of the callable or class
+    :rtype: str
+    """
     return getattr(fn_or_class, "__name__", fn_or_class.__class__.__name__).lower()
 
 
 def get_callable_dict(fn: Union[Callable, Mapping, Sequence]) -> Union[Dict, Mapping]:
+    """
+    Creates a dictionary with the name of the callable as key and the callable as value.
+
+    :param fn: Callable, sequence or mapping we want to convert to a dictionary
+    :type fn: Union[Callable, Mapping, Sequence]
+    :return: A dictionary with the name of the callable as key and the callable as value
+    :rtype: Union[Dict, Mapping]
+    """
     if isinstance(fn, Mapping):
         return fn
     elif isinstance(fn, Sequence):
