7fa7613b441d440a67da7ddce1ffcd6283de05d3
Lars Voegtlin <lars.voegtlin@unifr.ch>
2024-02-13 14:48:06 +0100

    :construction: created transformation and test but not run yet

--- src/datamodules/utils/single_transforms.py
+++ src/datamodules/utils/single_transforms.py
@@ -1,4 +1,8 @@
+from typing import List
+
 import math
+import torch
+from torchvision.transforms import functional
 
 import src.datamodules.utils.functional
 
@@ -6,3 +10,17 @@ import src.datamodules.utils.functional
 class OneHotToPixelLabelling(object):
     def __call__(self, tensor):
         return src.datamodules.utils.functional.argmax_onehot(tensor)
+
+
+class RightAngleRotation:
+    def __init__(self, angle_list: List[int] = [0, 90, 180, 270]):
+        self.angles = angle_list
+        self.target_class = torch.randint(low=0, high=len(self.angles), size=(1,))
+
+    def _update_target_class(self) -> None:
+        self.target_class = torch.randint(low=0, high=len(self.angles), size=(1,))
+
+    def __call__(self, tensor):
+        rotation_angle = self.angles[self.target_class]
+        self._update_target_class()
+        return functional.rotate(img=tensor, angle=rotation_angle)
--- tests/datamodules/util/test_single_transforms.py
+++ tests/datamodules/util/test_single_transforms.py
@@ -1,6 +1,6 @@
 import torch
 
-from src.datamodules.utils.single_transforms import OneHotToPixelLabelling
+from src.datamodules.utils.single_transforms import OneHotToPixelLabelling, RightAngleRotation
 
 
 def test_one_hot_to_pixel_labelling():
@@ -14,3 +14,26 @@ def test_one_hot_to_pixel_labelling():
     trans_output = transformation(tensor_input)
     assert isinstance(trans_output, torch.Tensor)
     assert torch.equal(trans_output, torch.tensor([[2, 0], [0, 2]]))
+
+
+def test_right_angle_rotation():
+    torch.manual_seed(1)
+    transformation = RightAngleRotation()
+    tensor_input = torch.Tensor([[[0.5, 0.5],
+                                  [0.1, 0.1]],
+                                 [[0.2, 0.2],
+                                  [0.9, 0.9]]])
+    trans_output = transformation(tensor=tensor_input)
+    assert torch.eq(transformation.target_class, torch.Tensor([1]))
+    assert torch.eq(trans_output, torch.Tensor([[[0.2, 0.5],
+                                                 [0.9, 0.1]],
+                                                [0.2, 0.5],
+                                                [0.9, 0.1]]))
+
+
+def test__update_target_class():
+    torch.manual_seed(1)  # 1, 2, 0
+    transformation = RightAngleRotation()
+    assert torch.eq(transformation.target_class, torch.Tensor([1]))
+    transformation._update_target_class()
+    assert torch.eq(transformation.target_class, torch.Tensor([2]))
