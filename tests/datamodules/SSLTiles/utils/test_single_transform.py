bb453f48f09928f0e780d5af60d403120664d998
Lars Voegtlin <lars.voegtlin@unifr.ch>
2024-02-15 15:21:41 +0100

    :white_check_mark: singel transform tests

--- tests/datamodules/SSLTiles/utils/test_single_transform.py
+++ tests/datamodules/SSLTiles/utils/test_single_transform.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pytest
 from torchvision.datasets.folder import pil_loader
 from torchvision.transforms import ToTensor
@@ -7,14 +8,23 @@ from src.datamodules.SSLTiles.utils.single_transform import TilesBuilding
 from tests.test_data.dummy_data_histdb_new.dummy_data import data_dir
 
 
-def test_tiles_transformation_fail_check(data_dir):
+@pytest.fixture()
+def pil_image(data_dir):
+    return pil_loader(data_dir / 'train' / 'data' / 'fmb-cb-55-005v.png')
+
+
+def test_tiles_transformation_fail_too_many_fixed_positions(data_dir):
     with pytest.raises(ValueError):
         _ = TilesBuilding(rows=2, cols=2, fixed_positions=3, width_center_crop=840, height_center_crop=1200)
 
 
-def test_tiles_transformation(data_dir):
-    img = pil_loader(data_dir / 'train' / 'data' / 'fmb-cb-55-005v.png')
-    img_tensor = ToTensor()(img)
+def test_tiles_transformation_fail_fixed_positions_too_big(data_dir):
+    with pytest.raises(ValueError):
+        _ = TilesBuilding(rows=2, cols=2, fixed_positions=5, width_center_crop=840, height_center_crop=1200)
+
+
+def test_tiles_transformation(pil_image):
+    img_tensor = ToTensor()(pil_image)
     transform = TilesBuilding(rows=3, cols=2, fixed_positions=3, width_center_crop=840, height_center_crop=1200)
     assert transform(img_tensor).shape == img_tensor.shape
     assert transform.rows == 3
@@ -30,16 +40,29 @@ def test_tiles_transformation(data_dir):
     assert transform.target_class is not None
 
 
-@pytest.mark.skip('under construction')
+def test_tiles_transformation_call(mocker, pil_image):
+    img_tensor = ToTensor()(pil_image)
+    transform = TilesBuilding(rows=3, cols=2, fixed_positions=3, width_center_crop=840, height_center_crop=1200)
+    mocker.patch('numpy.random.randint', return_value=0)
+    transform(img_tensor)
+    assert transform.target_class == 0
+
+
 def test__update_target_perm():
-    assert False
+    transform = TilesBuilding(rows=3, cols=2, fixed_positions=3, width_center_crop=840, height_center_crop=1200)
+    assert transform.target_class is None
+    transform._update_target_perm()
+    assert transform.target_class is not None
 
 
-@pytest.mark.skip('under construction')
 def test__get_perms_with_n_fixed_positions():
-    assert False
+    transform = TilesBuilding(rows=2, cols=2, fixed_positions=2, width_center_crop=840, height_center_crop=1200)
+    assert np.all(np.equal(transform._get_perms_with_n_fixed_positions(), np.array(
+        [[0, 1, 3, 2], [0, 2, 1, 3], [0, 3, 2, 1], [1, 0, 2, 3], [2, 1, 0, 3], [3, 1, 2, 0]])))
 
 
-@pytest.mark.skip('under construction')
-def test__get_tile_image():
-    assert False
+def test__get_tile_image(pil_image):
+    transform = TilesBuilding(rows=2, cols=2, fixed_positions=2, width_center_crop=840, height_center_crop=1200)
+    img_tensor = ToTensor()(pil_image)
+    trans_ig = transform(img_tensor)
+    assert trans_ig.shape == img_tensor.shape
