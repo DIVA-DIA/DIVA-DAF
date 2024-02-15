e14a606ecf41bfdc350b5be40950647240147253
Lars Voegtlin <lars.voegtlin@unifr.ch>
2024-02-15 16:56:03 +0100

    :recycle: removed more code duplications

--- src/tasks/utils/outputs.py
+++ src/tasks/utils/outputs.py
@@ -1,5 +1,7 @@
 from typing import Dict, List
 
+import numpy
+import numpy as np
 from pytorch_lightning.utilities import LightningEnum
 
 
@@ -15,3 +17,18 @@ class OutputKeys(LightningEnum):
 
 def reduce_dict(input_dict: Dict, key_list: List) -> Dict:
     return {key: input_dict[key] for key in key_list if key in input_dict}
+
+
+def save_numpy_files(trainer, test_output_path, input_idx, output):
+    if not hasattr(trainer.datamodule, 'get_img_name_coordinates'):
+        raise NotImplementedError('Datamodule does not provide detailed information of the crop')
+    for patch, idx in zip(output[OutputKeys.PREDICTION].detach().cpu().numpy(),
+                          input_idx.detach().cpu().numpy()):
+        patch_info = trainer.datamodule.get_img_name_coordinates(idx)
+        img_name = patch_info[0]
+        patch_name = patch_info[1]
+        dest_folder = test_output_path / 'patches' / img_name
+        dest_folder.mkdir(parents=True, exist_ok=True)
+        dest_filename = dest_folder / f'{patch_name}.npy'
+
+        np.save(file=str(dest_filename), arr=patch)
--- src/tasks/utils/task_utils.py
+++ src/tasks/utils/task_utils.py
@@ -1,5 +1,11 @@
 from typing import Callable, Mapping, Sequence, Dict, Union
 
+import pytorch_lightning
+
+from utils import utils
+
+log = utils.get_logger(__name__)
+
 
 # inspired by https://github.com/PyTorchLightning/lightning-flash/blob/2ec52e633bb3679f50dd7e30526885a4547e1851/flash/core/utilities/apply_func.py
 def get_callable_name(fn_or_class: Union[Callable, Sequence, object]) -> str:
@@ -13,3 +19,14 @@ def get_callable_dict(fn: Union[Callable, Mapping, Sequence]) -> Union[Dict, Map
         return {get_callable_name(f): f for f in fn}
     elif callable(fn):
         return {get_callable_name(fn): fn}
+
+
+def print_merge_tool_info(trainer: 'pytorch_lightning.Trainer', test_output_path: str, data_format: str):
+    datamodule_path = trainer.datamodule.data_dir
+    prediction_path = (test_output_path / 'patches').absolute()
+    output_path = (test_output_path / 'result').absolute()
+    data_folder_name = trainer.datamodule.data_folder_name
+    gt_folder_name = trainer.datamodule.gt_folder_name
+    log.info(f'To run the merging of patches:')
+    log.info(f'python tools/merge_cropped_output_{data_format}.py -d {datamodule_path} -p {prediction_path} '
+             f'-o {output_path} -df {data_folder_name} -gf {gt_folder_name}')
