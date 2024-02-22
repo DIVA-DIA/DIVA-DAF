826e23796a882a5120890bb189e1e19f60e925cf
Lars Voegtlin <lars.voegtlin@unifr.ch>
2024-02-22 14:33:30 +0100

    :art: improved some of the code and replaced duplication with method calls

--- src/datamodules/RotNet/utils/image_analytics.py
+++ src/datamodules/RotNet/utils/image_analytics.py
@@ -7,6 +7,7 @@ from typing import Any, Dict
 
 import numpy as np
 
+from datamodules.utils.misc import check_missing_analytics, save_json
 from src.datamodules.utils.image_analytics import compute_mean_std
 
 
@@ -33,32 +34,17 @@ def get_analytics_data(input_path: Path, data_folder_name: str, get_gt_data_path
 
     analytics_path_data = input_path / f'analytics.data.{data_folder_name}.json'
 
-    analytics_data = None
+    analytics_data, missing_analytics_data = check_missing_analytics(analytics_path_data, expected_keys_data)
 
-    missing_analytics_data = True
+    if not missing_analytics_data:
+        return analytics_data
+    train_path = input_path / 'train'
+    gt_data_path_list = get_gt_data_paths_func(train_path, data_folder_name=data_folder_name, gt_folder_name=None)
 
-    if analytics_path_data.exists():
-        with analytics_path_data.open(mode='r') as f:
-            analytics_data = json.load(fp=f)
-        # check if analytics file is complete
-        if all(k in analytics_data for k in expected_keys_data):
-            missing_analytics_data = False
-
-    if missing_analytics_data:
-        train_path = input_path / 'train'
-        gt_data_path_list = get_gt_data_paths_func(train_path, data_folder_name=data_folder_name, gt_folder_name=None)
-
-        mean, std = compute_mean_std(file_names=gt_data_path_list, inmem=inmem, workers=workers)
-        analytics_data = {'mean': mean.tolist(),
-                          'std': std.tolist()}
-        # save json
-        try:
-            with analytics_path_data.open(mode='w') as f:
-                json.dump(obj=analytics_data, fp=f)
-        except IOError as e:
-            if e.errno == errno.EACCES:
-                logging.warning(f'WARNING: No permissions to write analytics file ({analytics_path_data})')
-            else:
-                raise
+    mean, std = compute_mean_std(file_names=gt_data_path_list, inmem=inmem, workers=workers)
+    analytics_data = {'mean': mean.tolist(),
+                      'std': std.tolist()}
+    # save json
+    save_json(analytics_data, analytics_path_data)
 
     return analytics_data
