e2c018b3b2466c7fd161a8bbbbf316ebcac0a60a
Lars Voegtlin <lars.voegtlin@unifr.ch>
2024-02-28 09:45:28 +0100

    :sparkles: added finished timetracker but no test yet

--- src/callbacks/general_callbacks.py
+++ src/callbacks/general_callbacks.py
@@ -1,4 +1,4 @@
-from datetime import time
+from time import time
 
 from pytorch_lightning.utilities import rank_zero_only
 from pytorch_lightning import Callback
@@ -15,21 +15,19 @@ class TimeTracker(Callback):
         self.start_time_train = time()
 
     @rank_zero_only
-    def on_train_end(self, trainer, pl_module):
-        self.log("train/total_time", time() - self.start_time_train)
-
-    @rank_zero_only
     def on_train_epoch_start(self, trainer, pl_module):
         self.start_time_train_epoch = time()
 
     @rank_zero_only
     def on_train_epoch_end(self, trainer, pl_module):
+        if trainer.current_epoch == trainer.max_epochs - 1:
+            self.log("train/total_time", time() - self.start_time_train)
         self.log("train/epoch_time", time() - self.start_time_train_epoch)
 
     @rank_zero_only
-    def on_test_start(self, trainer, pl_module):
+    def on_test_epoch_start(self, trainer, pl_module):
         self.start_time_test = time()
 
     @rank_zero_only
-    def on_test_end(self, trainer, pl_module):
+    def on_test_epoch_end(self, trainer, pl_module):
         self.log("test/total_time", time() - self.start_time_test)
