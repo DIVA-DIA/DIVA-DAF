40c6404f39b2b34c133addfa97f9048cd3c66c77
Lars Voegtlin <lars.voegtlin@unifr.ch>
2024-07-02 15:51:57 +0200

    :white_check_mark:

--- tests/datamodules/test_base_datamodule.py
+++ tests/datamodules/test_base_datamodule.py
@@ -1,4 +1,5 @@
 import pytest
+import re
 from src.datamodules.base_datamodule import AbstractDatamodule
 
 
@@ -20,8 +21,13 @@ def test_check_min_num_samples_no_drop_error(caplog):
     AbstractDatamodule.check_min_num_samples(num_devices=4, batch_size_input=batch_size, num_samples=num_samples,
                                              data_split=data_split,
                                              drop_last=False)
-    assert f'WARNING  src.datamodules.base_datamodule:rank_zero.py:24 ' \
-           f'#samples ({num_samples}) in "{data_split}" smaller than ' \
-           f'#devices (4) times batch size ({batch_size}). ' \
-           f'This works due to drop_last=False, however samples might occur multiple times. ' \
-           f'Check if this behavior is intended!\n' in caplog.text
+    expect_base = fr'WARNING  src\.datamodules\.base_datamodule:rank_zero.py:\d\d '
+    expect_1 = f'#samples ({num_samples}) in "{data_split}" smaller than '
+    expect_2 = f'#devices (4) times batch size ({batch_size}). '
+    expect_3 = f'This works due to drop_last=False, however samples might occur multiple times. '
+    expect_4 = f'Check if this behavior is intended!\n'
+    assert re.search(expect_base, caplog.text)
+    assert expect_1 in caplog.text
+    assert expect_2 in caplog.text
+    assert expect_3 in caplog.text
+    assert expect_4 in caplog.text
