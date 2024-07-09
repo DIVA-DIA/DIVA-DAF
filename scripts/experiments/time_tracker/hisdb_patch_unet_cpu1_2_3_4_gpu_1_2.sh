d9858c6384a8fc8d298a99a1db71849530defb9e
Lars Voegtlin <lars.voegtlin@unifr.ch>
2024-07-09 15:28:46 +0200

    :art: improved permutation filter

--- scripts/experiments/time_tracker/hisdb_patch_unet_cpu1_2_3_4_gpu_1_2.sh
+++ scripts/experiments/time_tracker/hisdb_patch_unet_cpu1_2_3_4_gpu_1_2.sh
@@ -3,11 +3,11 @@
 set -e
 
 #gpu
-python run.py experiment=time_taking_hisdb_patch_unet.yaml trainer.devices="[1]" name="time_tracking_hisdb_cropped_unet_gpu_1"
-python run.py experiment=time_taking_hisdb_patch_unet.yaml trainer.devices="[1,2]" name="time_tracking_hisdb_cropped_unet_gpu_2"
+#python run.py experiment=time_taking_hisdb_patch_unet.yaml trainer.devices="[1]" name="time_tracking_hisdb_cropped_unet_gpu_1"
+#python run.py experiment=time_taking_hisdb_patch_unet.yaml trainer.devices="[1,2]" name="time_tracking_hisdb_cropped_unet_gpu_2"
 
 # cpu
-python run.py experiment=time_taking_hisdb_patch_unet.yaml trainer.devices="1" trainer.accelerator="cpu" name="time_tracking_hisdb_cropped_unet_cpu_1"
+#python run.py experiment=time_taking_hisdb_patch_unet.yaml trainer.devices="1" trainer.accelerator="cpu" name="time_tracking_hisdb_cropped_unet_cpu_1"
 python run.py experiment=time_taking_hisdb_patch_unet.yaml trainer.devices="2" trainer.accelerator="cpu" name="time_tracking_hisdb_cropped_unet_cpu_2"
 python run.py experiment=time_taking_hisdb_patch_unet.yaml trainer.devices="4" trainer.accelerator="cpu" name="time_tracking_hisdb_cropped_unet_cpu_4"
 python run.py experiment=time_taking_hisdb_patch_unet.yaml trainer.devices="8" trainer.accelerator="cpu" name="time_tracking_hisdb_cropped_unet_cpu_8"
